1cd2a4b00fe4bd383d3842e4e3833bd09af9e04f
SamyakJain2020 <samyakjainking@gmail.com>
2022-05-03 07:03:35 +0530

    cors commit

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express();
+const cors = require('cors');
 
 dotenv.config({ path: './config.env' })
 require('./db/conn')
-
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());        // to convert data in form of json
 
 // const User = require('./model/userSchema')
